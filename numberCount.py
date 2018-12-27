2506fc831ba3187f463b39a0693476170ae5f7a7
Angphurba Sherpa <angphurba.sherpa@outlook.com>
2018-12-27 09:43:29 -0500

    Add files via upload

---
+++ numberCount.py
@@ -0,0 +1,9 @@
+num_a = int(input('Enter number that you want to count down to zero:'))
+num_b = int(input('Enter number that you want to count up starting at 0:'))
+a = -1
+while a < num_a:
+    a += 1
+    print (a)
+user = range (0,num_b+1)
+for count in user:
+    print (count)
